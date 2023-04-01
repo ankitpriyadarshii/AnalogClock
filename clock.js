bc23c314e059d9b86ad45c46cc08c6549e5ee636
ANKIT PRIYADARSHI <72643034+ankitpriyadarshii@users.noreply.github.com>
2023-04-01 11:09:07 +0530

    Add files via upload

---
+++ clock.js
@@ -0,0 +1,16 @@
+// To do it every given interval (1000ms or 1 sec in our case)
+setInterval(() => {
+    d = new Date();
+    htime = d.getHours();
+    mtime = d.getMinutes();
+    stime = d.getSeconds();
+
+    // Formulae for rotations of hands
+    hrotation = 30*htime + mtime/2;
+    mrotation = 6*mtime;
+    srotation = 6*stime;
+
+    hour.style.transform = `rotate(${hrotation}deg)`;
+    minute.style.transform = `rotate(${mrotation}deg)`;
+    second.style.transform = `rotate(${srotation}deg)`;
+}, 1000);
